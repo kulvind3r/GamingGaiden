117da532494ee237ce603721a1f69ee248960623
Kulvinder Singh <kulvind3r.singh@gmail.com>
2024-04-18 16:02:24 +0530

    Label data in most played chart

--- ui/resources/js/MostPlayed.js
+++ ui/resources/js/MostPlayed.js
@@ -8,12 +8,12 @@ function updateChart(gameCount) {
     
     let labels = [];
     let data = [];
-
+    
     if (gameCount > gamingData.length)
     {
-      gameCount = gamingData.length
+        gameCount = gamingData.length
     }
-
+    
     for (i=0; i<gameCount; i++)
     {
         labels.push(gamingData[i].name)
@@ -23,15 +23,16 @@ function updateChart(gameCount) {
     if (chart) {
         chart.destroy();
     }
-
+    
     const ctx = document.getElementById('most-played-chart').getContext('2d');
     
     chart = new Chart(ctx, {
         type: 'bar',
+        plugins: [ChartDataLabels],
         data: {
             labels: labels,
             datasets: [{
-                label: 'Game Time (hours)',
+                label: 'Playtime (Hours)',
                 data: data.map(row => row.time),
                 borderWidth: 2
             }]
@@ -40,25 +41,42 @@ function updateChart(gameCount) {
             indexAxis: 'y',
             scales: {
                 y: {
-                  ticks: {
-                    autoSkip: false
-                  }
+                    ticks: {
+                        autoSkip: false
+                    }
                 },
                 x: {
                     ticks: {
-                      stepSize: 10
+                        stepSize: 10
                     }
-                  }
-              },
+                }
+            },
             elements: {
                 bar: {
-                  borderWidth: 1,
+                    borderWidth: 1,
                 }
-              },
+            },
             responsive: true, // Make the chart responsive
             plugins: {
+                tooltip: {
+                    enabled: false
+                },
                 legend: {
-                  position: 'right',
+                    onClick: null,
+                    labels: {
+                        font: {
+                            size: 15,
+                            family: 'monospace'
+                        }
+                    }
+                },
+                datalabels: {
+                    anchor: "end",
+                    align: "right",
+                    color: '#000000',
+                    font: {
+                        family: 'monospace'
+                    }
                 },
             },
             maintainAspectRatio: true
@@ -69,19 +87,19 @@ function updateChart(gameCount) {
 function loadDataFromTable() {
     const table = document.getElementById('data-table');
     const rows = table.querySelectorAll('tbody tr');
-
+    
     gamingData = Array.from(rows).map(row => {
         const name = row.cells[0].textContent;
         const time = parseFloat(row.cells[1].textContent);
         return { name, time };
     });
-
+    
     // Remove header row data
     gamingData.shift()
-  
+    
     selectBox = document.getElementById('game-count');
     const maxOptions = Math.min(50, gamingData.length);
-
+    
     // Loop to generate options
     for (let i = 10; i <= maxOptions; i += 10) {
         const option = document.createElement('option');
@@ -89,7 +107,7 @@ function loadDataFromTable() {
         option.text = i;
         selectBox.add(option);
     }
-
+    
     if (gamingData.length < 50) {
         const showAllOption = document.createElement('option');
         showAllOption.value = gamingData.length;
