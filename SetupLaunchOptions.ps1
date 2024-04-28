79b58fb4a1982b834f32f71c2138cd375f76be33
Kulvinder Singh <kulvind3r.singh@gmail.com>
2024-04-28 15:16:49 +0530

    Simpler auto start mechaism using shell:startup

--- SetupLaunchOptions.ps1
+++ SetupLaunchOptions.ps1
@@ -3,7 +3,8 @@ param(
 	[string]$InstallDirectory
 ) 
 
-Set-Location $InstallDirectory 
+Set-Location $InstallDirectory
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
 
 function UserPrompt($Msg, $Color = "Green") {
     Write-Host $Msg -ForegroundColor $Color
@@ -15,7 +16,6 @@ function CreateShortcut() {
     $WorkingDirectory = $GamingGaidenScript.Directory.FullName
 
     $IconPath = (Get-Item ".\icons\running.ico").FullName
-    $DesktopPath = [Environment]::GetFolderPath("Desktop")
 
     $shortcut = (New-Object -ComObject Wscript.Shell).CreateShortcut("$DesktopPath\Gaming Gaiden.lnk")
     $shortcut.TargetPath = 'powershell'
@@ -28,21 +28,10 @@ function CreateShortcut() {
     Copy-Item "$DesktopPath\Gaming Gaiden.lnk" "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\"
 }
 
-function CreateScheduledTask() {
-    $GamingGaidenScript = (Get-Item ".\GamingGaiden.ps1")
-    $GamingGaidenPath = $GamingGaidenScript.FullName
-    $WorkingDirectory = $GamingGaidenScript.Directory.FullName
+function SetupAutoStart() {
+    $StartupPath = [Environment]::GetFolderPath('Startup')
 
-    $action = New-ScheduledTaskAction -Execute 'powershell' -Argument "-NoLogo -ExecutionPolicy bypass -WindowStyle hidden -File `"$GamingGaidenPath`""
-    $action.WorkingDirectory = "$WorkingDirectory"
-
-    $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -ExecutionTimeLimit 0
-
-    $trigger = New-ScheduledTaskTrigger -AtLogOn
-    $trigger.Delay = 'PT1M'
-
-    # Register the scheduled task
-    Register-ScheduledTask -TaskName "Gaming Gaiden Autostart" -Description "Runs Gaming Gaiden at startup" -Action $action -Trigger $trigger -Settings $settings -Force
+    Copy-Item "$DesktopPath\Gaming Gaiden.lnk" "$StartupPath\"
 }
 
 UserPrompt "Creating Shortcuts"
@@ -51,13 +40,13 @@ CreateShortcut
 UserPrompt "Unblocking all Gaming Gaiden files"
 Get-ChildItem . -recurse | Unblock-File
 
-$ScheduledTaskChoice = Read-Host -Prompt "Would you like Gaming Gaiden to auto start at boot? Yes/No"
-if ( $ScheduledTaskChoice.ToLower() -match 'yes|y' ) { 
-    CreateScheduledTask
-    UserPrompt "Auto start scheduled task successfully created."
+$AutoStartChoice = Read-Host -Prompt "Would you like Gaming Gaiden to auto start at boot? Yes/No"
+if ( $AutoStartChoice.ToLower() -match 'yes|y' ) { 
+    SetupAutoStart
+    UserPrompt "Auto start successfully setup."
 }
 else {
-    UserPrompt "Auto start scheduled task setup cancelled by user."
+    UserPrompt "Auto start setup cancelled by user."
 }
 UserPrompt "Installation successful. Press any key to exit..."
 $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
