76b0b4484b576120c1cbc4e4e198cebe88c6cde8
Kulvinder Singh <kulvind3r.singh@gmail.com>
2024-04-28 13:47:04 +0530

    Set y scale to log with base 2 in summary view

--- ui/resources/js/Summary.js
+++ ui/resources/js/Summary.js
@@ -5,6 +5,77 @@ let inProgressCount = 0;
 
 $('table')[0].setAttribute('id','data-table');
 
+// Create custom log axis in base 2
+class Log2Axis extends Chart.Scale {
+    constructor(cfg) {
+      super(cfg);
+      this._startValue = undefined;
+      this._valueRange = 0;
+    }
+  
+    parse(raw, index) {
+      const value = Chart.LinearScale.prototype.parse.apply(this, [raw, index]);
+      return isFinite(value) && value > 0 ? value : null;
+    }
+  
+    determineDataLimits() {
+      const {
+        min,
+        max
+      } = this.getMinMax(true);
+      this.min = isFinite(min) ? Math.max(0, min) : null;
+      this.max = isFinite(max) ? Math.max(0, max) : null;
+    }
+  
+    buildTicks() {
+      const ticks = [];
+  
+      let power = Math.floor(Math.log2(this.min || 1));
+      let maxPower = Math.ceil(Math.log2(this.max || 2));
+      while (power <= maxPower) {
+        ticks.push({
+          value: Math.pow(2, power)
+        });
+        power += 1;
+      }
+  
+      this.min = ticks[0].value;
+      this.max = ticks[ticks.length - 1].value;
+      return ticks;
+    }
+  
+    /**
+     * @protected
+     */
+    configure() {
+      const start = this.min;
+  
+      super.configure();
+  
+      this._startValue = Math.log2(start);
+      this._valueRange = Math.log2(this.max) - Math.log2(start);
+    }
+  
+    getPixelForValue(value) {
+      if (value === undefined || value === 0) {
+        value = this.min;
+      }
+  
+      return this.getPixelForDecimal(value === this.min ? 0 :
+        (Math.log2(value) - this._startValue) / this._valueRange);
+    }
+  
+    getValueForPixel(pixel) {
+      const decimal = this.getDecimalForPixel(pixel);
+      return Math.pow(2, this._startValue + decimal * this._valueRange);
+    }
+}
+
+Log2Axis.id = 'log2';
+Log2Axis.defaults = {};
+
+Chart.register(Log2Axis);
+
 function updateChart() {
 
     const ctx = document.getElementById('session-vs-playtime-chart').getContext('2d');
@@ -31,6 +102,7 @@ function updateChart() {
         options: {
             scales: {
                 y: {
+                    type: 'log2',
                     title: {
                         display: true,
                         text: "PlayTime (Hours)",
@@ -38,14 +110,12 @@ function updateChart() {
                             size: 18,
                             family: 'monospace'
                         }
-                    },
-                    ticks: {
-                        stepSize: 10
                     }
                 },
                 // Alignment Hack: Add an identical y scale on right side, to center the graph on page.
                 // Then hide the right side scale by setting ticks and title color identical to background.
                 yRight: {
+                    type: 'log2',
 					position: 'right',
 					grid: {
 						display: false
@@ -73,7 +143,7 @@ function updateChart() {
                         }
                     },
                     ticks: {
-                        stepSize: 5
+                        stepSize: 10
                     }
                 }
             },
