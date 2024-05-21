99564e46acebb2c1f120f733e98f9c847d04ee51
Kulvinder Singh <kulvind3r.singh@gmail.com>
2024-05-21 11:59:11 +0530

    Format and cleanup ProcessFunctions module

--- modules/ProcessFunctions.psm1
+++ modules/ProcessFunctions.psm1
@@ -1,136 +1,135 @@
 function DetectGame() {
-	Log "Starting game detection"
+    Log "Starting game detection"
 
-    $GetGameExesQuery = "SELECT exe_name FROM games ORDER BY last_play_date DESC"
-	$GetEmulatorExesQuery = "SELECT exe_name FROM emulated_platforms"
+    $getGameExesQuery = "SELECT exe_name FROM games ORDER BY last_play_date DESC"
+    $getEmulatorExesQuery = "SELECT exe_name FROM emulated_platforms"
 
-    $GameExeList = (RunDBQuery $GetGameExesQuery).exe_name
-	
-	$RawEmulatorExes = (RunDBQuery $GetEmulatorExesQuery).exe_name
-	# Flatten the rows with multiple exes into a single list
-	$EmulatorExeList = ($RawEmulatorExes -join ',') -split ','
+    $gameExeList = (RunDBQuery $getGameExesQuery).exe_name
+    
+    $rawEmulatorExes = (RunDBQuery $getEmulatorExesQuery).exe_name
+    # Flatten the rows with multiple exes into a single list
+    $emulatorExeList = ($rawEmulatorExes -join ',') -split ','
 
-	$ExesToDetect = $($GameExeList; $EmulatorExeList) | Select-Object -Unique
+    $exesToDetect = $($gameExeList; $emulatorExeList) | Select-Object -Unique
 
     do {
-		$AllProcesses = ((Get-Process).ProcessName | Select-Object -Unique)
-        foreach ( $ExeName in $ExesToDetect ){
-			if ( $AllProcesses -contains $ExeName )
-			{
-				Log "Found $ExeName running. Exiting detection"
-				return $ExeName
-			}
-		}
-		Clear-Variable AllProcesses; Remove-Variable AllProcesses
-		# Mandatory Garbage collect in loop because powershell is dogshit in recovering memory from infinite loops
-		[System.GC]::GetTotalMemory($true) | out-null
+        $allProcesses = ((Get-Process).ProcessName | Select-Object -Unique)
+        foreach ( $exeName in $exesToDetect ) {
+            if ( $allProcesses -contains $exeName ) {
+                Log "Found $exeName running. Exiting detection"
+                return $exeName
+            }
+        }
+        Clear-Variable allProcesses; Remove-Variable allProcesses
+        # Mandatory Garbage collect in loop because powershell is dogshit in recovering memory from infinite loops
+        [System.GC]::GetTotalMemory($true) | out-null
         Start-Sleep -s 5
     }
     while ($true)
 }
 
 function TimeTrackerLoop($DetectedExe) {
-	$HWinfoSensorSession = 'HKCU:\SOFTWARE\HWiNFO64\Sensors\Custom\Gaming Gaiden\Other1'
-	$PlayTimeForCurrentSession = 0
-	$IdleSessionsCount = 0
-	$IdleSessions = New-Object int[] 100;
-	$ExeStartTime = (Get-Process $DetectedExe).StartTime | Sort-Object | Select-Object -First 1
-    while(Get-Process $DetectedExe)
-    {
-        $PlayTimeForCurrentSession = [int16] (New-TimeSpan -Start $ExeStartTime).TotalMinutes
-		$IdleTime = [int16] ([PInvoke.Win32.UserInput]::IdleTime).Minutes
-		if ($IdleTime -ge 10)
-		{
-			# Entered idle Session
-			while( $IdleTime -ge 5)
-			{
-				# Track idle Time for current Idle Session
-				$IdleSessions[$IdleSessionsCount] = $IdleTime
-				$IdleTime = [int16] ([PInvoke.Win32.UserInput]::IdleTime).Minutes
+    $hwInfoSensorSession = 'HKCU:\SOFTWARE\HWiNFO64\Sensors\Custom\Gaming Gaiden\Other1'
+    $playTimeForCurrentSession = 0
+    $idleSessionsCount = 0
+    $idleSessions = New-Object int[] 100;
+    $exeStartTime = (Get-Process $DetectedExe).StartTime | Sort-Object | Select-Object -First 1
 
-				# Keep the hwinfo sensor updated to current play time session length while tracking idle session
-				$PlayTimeForCurrentSession = [int16] (New-TimeSpan -Start $ExeStartTime).TotalMinutes
-				Set-Itemproperty -path $HWinfoSensorSession -Name 'Value' -value $PlayTimeForCurrentSession
+    while (Get-Process $DetectedExe) {
+        $playTimeForCurrentSession = [int16] (New-TimeSpan -Start $exeStartTime).TotalMinutes
+        $idleTime = [int16] ([PInvoke.Win32.UserInput]::IdleTime).Minutes
 
-				Start-Sleep -s 5
-			}
-			# Exited Idle Session, increment idle session counter for storing next idle sessions's length
-			$IdleSessionsCount++
-		}
-		Set-Itemproperty -path $HWinfoSensorSession -Name 'Value' -value $PlayTimeForCurrentSession
+        if ($idleTime -ge 10) {
+            # Entered idle Session
+            while ( $idleTime -ge 5) {
+                # Track idle Time for current Idle Session
+                $idleSessions[$idleSessionsCount] = $idleTime
+                $idleTime = [int16] ([PInvoke.Win32.UserInput]::IdleTime).Minutes
+
+                # Keep the hwinfo sensor updated to current play time session length while tracking idle session
+                $playTimeForCurrentSession = [int16] (New-TimeSpan -Start $exeStartTime).TotalMinutes
+                Set-Itemproperty -path $hwInfoSensorSession -Name 'Value' -value $playTimeForCurrentSession
+
+                Start-Sleep -s 5
+            }
+            # Exited Idle Session, increment idle session counter for storing next idle sessions's length
+            $idleSessionsCount++
+        }
+
+        Set-Itemproperty -path $hwInfoSensorSession -Name 'Value' -value $playTimeForCurrentSession
         Start-Sleep -s 5
     }
-	$IdleTimeForCurrentSession = ($IdleSessions | Measure-Object -Sum).Sum
-	Log "Play time for current session: $PlayTimeForCurrentSession min. Idle time for current session: $IdleTimeForCurrentSession min."
-	$PlayTimeExcludingIdleTime = $PlayTimeForCurrentSession - $IdleTimeForCurrentSession
-	Log "Play time for current session excluding Idle time $PlayTimeExcludingIdleTime min"
-	return @($PlayTimeExcludingIdleTime, $IdleTimeForCurrentSession)
+
+    $idleTimeForCurrentSession = ($idleSessions | Measure-Object -Sum).Sum
+    Log "Play time for current session: $playTimeForCurrentSession min. Idle time for current session: $idleTimeForCurrentSession min."
+
+    $PlayTimeExcludingIdleTime = $playTimeForCurrentSession - $idleTimeForCurrentSession
+    Log "Play time for current session excluding Idle time $PlayTimeExcludingIdleTime min"
+
+    return @($PlayTimeExcludingIdleTime, $idleTimeForCurrentSession)
 }
 
 function MonitorGame($DetectedExe) {
-	Log "Starting monitoring for $DetectedExe"
-	
-	$DatabaseFileHashBefore = CalculateFileHash '.\GamingGaiden.db'
-	Log "Database hash before: $DatabaseFileHashBefore"
+    Log "Starting monitoring for $DetectedExe"
+    
+    $databaseFileHashBefore = CalculateFileHash '.\GamingGaiden.db'
+    Log "Database hash before: $databaseFileHashBefore"
 
-	$EmulatedGameDetails = $null
-	$GameName = $null
-	$EntityFound = $null
-	$UpdatedPlayTime = 0
-	$UpdatedLastPlayDate = (Get-Date ([datetime]::UtcNow) -UFormat %s).Split('.').Get(0)
+    $emulatedGameDetails = $null
+    $gameName = $null
+    $entityFound = $null
+    $updatedPlayTime = 0
+    $updatedLastPlayDate = (Get-Date ([datetime]::UtcNow) -UFormat %s).Split('.').Get(0)
 
-	if (IsExeEmulator $DetectedExe)
-	{
-		$EmulatedGameDetails = findEmulatedGameDetails $DetectedExe
-		if ($EmulatedGameDetails -eq $false)
-		{
-			Log "Error: Problem in fetching emulated game details. See earlier logs for more info"
-			Log "Error: Cannot resume detection until $DetectedExe exits. No playtime will be recorded."
-			
-			TimeTrackerLoop $DetectedExe
-			return
-		}
-		$GameName = $EmulatedGameDetails.Name
-		$EntityFound = DoesEntityExists "games" "name" $GameName
-	}
-	else
-	{
-		$DetectedExePattern = SQLEscapedMatchPattern($DetectedExe.Trim())
-		$GetGameNameQuery = "SELECT name FROM games WHERE exe_name LIKE '{0}'" -f $DetectedExePattern
-		$EntityFound = RunDBQuery $GetGameNameQuery
-		$GameName = $EntityFound.name
-	}
-	
-	Write-Output "$GameName" > "$env:TEMP\GG-TrackingGame.txt"
-	$SessionTimeDetails = TimeTrackerLoop $DetectedExe
-    $CurrentPlayTime = $SessionTimeDetails[0]
-	$CurrentIdleTime = $SessionTimeDetails[1]
-	Remove-Item "$env:TEMP\GG-TrackingGame.txt"
+    if (IsExeEmulator $DetectedExe) {
+        $emulatedGameDetails = findEmulatedGameDetails $DetectedExe
+        if ($emulatedGameDetails -eq $false) {
+            Log "Error: Problem in fetching emulated game details. See earlier logs for more info"
+            Log "Error: Cannot resume detection until $DetectedExe exits. No playtime will be recorded."
+            
+            TimeTrackerLoop $DetectedExe
+            return
+        }
+        $gameName = $emulatedGameDetails.Name
+        $entityFound = DoesEntityExists "games" "name" $gameName
+    }
+    else {
+        $detectedExePattern = SQLEscapedMatchPattern($DetectedExe.Trim())
+        $getGameNameQuery = "SELECT name FROM games WHERE exe_name LIKE '{0}'" -f $detectedExePattern
+        $entityFound = RunDBQuery $getGameNameQuery
+        $gameName = $entityFound.name
+    }
+    
+    # Create Temp file to signal parent process to update notification icon color to show game is running
+    Write-Output "$gameName" > "$env:TEMP\GG-TrackingGame.txt"
+    $sessionTimeDetails = TimeTrackerLoop $DetectedExe
+    $currentPlayTime = $sessionTimeDetails[0]
+    $currentIdleTime = $sessionTimeDetails[1]
+    # Remove Temp file to signal parent process to update notification icon color to show game has finished
+    Remove-Item "$env:TEMP\GG-TrackingGame.txt"
 
-	if ($null -ne $EntityFound)
-	{
-		Log "Game Already Exists. Updating PlayTime and Last Played Date"
-		
-		$RecordedGamePlayTime = GetPlayTime $GameName
-		$RecordedGameIdleTime = GetIdleTime $GameName
-		$UpdatedPlayTime = $RecordedGamePlayTime + $CurrentPlayTime
-		$UpdatedIdleTime = $RecordedGameIdleTime + $CurrentIdleTime
-		
-		UpdateGameOnSession -GameName $GameName -GamePlayTime $UpdatedPlayTime -GameIdleTime $UpdatedIdleTime -GameLastPlayDate $UpdatedLastPlayDate
-	}
-	else
-	{
-		Log "Game Doesn't Exists. Adding New Game"
-		SaveGame -GameName $GameName -GameExeName $DetectedExe -GameIconPath "./icons/default.png" `
-				 -GamePlayTime $CurrentPlayTime -GameIdleTime $CurrentIdleTime -GameLastPlayDate $UpdatedLastPlayDate -GameCompleteStatus 'FALSE' -GamePlatform $EmulatedGameDetails.Platform -GameSessionCount 1
-	}
+    if ($null -ne $entityFound) {
+        Log "Game Already Exists. Updating PlayTime and Last Played Date"
+        
+        $recordedGamePlayTime = GetPlayTime $gameName
+        $recordedGameIdleTime = GetIdleTime $gameName
+        $updatedPlayTime = $recordedGamePlayTime + $currentPlayTime
+        $updatedIdleTime = $recordedGameIdleTime + $currentIdleTime
+        
+        UpdateGameOnSession -GameName $gameName -GamePlayTime $updatedPlayTime -GameIdleTime $updatedIdleTime -GameLastPlayDate $updatedLastPlayDate
+    }
+    else {
+        Log "Game Doesn't Exists. Adding New Game"
+        SaveGame -GameName $gameName -GameExeName $DetectedExe -GameIconPath "./icons/default.png" `
+            -GamePlayTime $currentPlayTime -GameIdleTime $currentIdleTime -GameLastPlayDate $updatedLastPlayDate -GameCompleteStatus 'FALSE' -GamePlatform $emulatedGameDetails.Platform -GameSessionCount 1
+    }
 
-	RecordPlaytimOnDate($CurrentPlayTime)
+    RecordPlaytimOnDate($currentPlayTime)
 
-	$DatabaseFileHashAfter = CalculateFileHash '.\GamingGaiden.db'
-	Log "Database hash after: $DatabaseFileHashAfter"
+    $databaseFileHashAfter = CalculateFileHash '.\GamingGaiden.db'
+    Log "Database hash after: $databaseFileHashAfter"
 
-	if ($DatabaseFileHashAfter -ne $DatabaseFileHashBefore){
+    if ($databaseFileHashAfter -ne $databaseFileHashBefore) {
         BackupDatabase
     }
 }
