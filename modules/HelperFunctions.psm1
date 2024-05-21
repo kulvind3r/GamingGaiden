7d3f0e53bdcf436fa93560984caf0d930b34206b
Kulvinder Singh <kulvind3r.singh@gmail.com>
2024-05-21 10:57:29 +0530

    Format and cleanup HelperFunctions module

--- modules/HelperFunctions.psm1
+++ modules/HelperFunctions.psm1
@@ -1,193 +1,192 @@
-function ResetLog($MSG) {
-	Remove-Item ".\GamingGaiden.log" -ErrorAction silentlycontinue
-	$Timestamp = Get-date -f s
-	Write-Output "$Timestamp : Cleared log at application boot" >> ".\GamingGaiden.log"
+function ResetLog() {
+    Remove-Item ".\GamingGaiden.log" -ErrorAction silentlycontinue
+    $timestamp = Get-date -f s
+    Write-Output "$timestamp : Cleared log at application boot" >> ".\GamingGaiden.log"
 }
 
 function Log($MSG) {
-	$Timestamp = Get-date -f s
-	$mutex = New-Object System.Threading.Mutex($false, "LogFileLock")
-	if($mutex.WaitOne(500)) {
-		Write-Output "$Timestamp : $MSG" >> ".\GamingGaiden.log"
-		[void]$mutex.ReleaseMutex()
-	}
+    $timestamp = Get-date -f s
+    $mutex = New-Object System.Threading.Mutex($false, "LogFileLock")
+
+    if ($mutex.WaitOne(500)) {
+        Write-Output "$timestamp : $MSG" >> ".\GamingGaiden.log"
+        [void]$mutex.ReleaseMutex()
+    }
 }
 
-function CleanupTempFiles(){
-	Remove-Item -Force "$env:TEMP\GG-*.png"
+function CleanupTempFiles() {
+    Remove-Item -Force "$env:TEMP\GG-*.png"
 }
 
 function SQLEscapedMatchPattern($pattern) {
-	return $pattern -replace "'", "''"
+    return $pattern -replace "'", "''"
 }
 
 function ToBase64($String) {
-	return [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($String))
+    return [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($String))
 }
 
 function PlayTimeMinsToString($PlayTime) {
-	$Minutes = $null; $Hours = [math]::divrem($PlayTime, 60, [ref]$Minutes);
-	return ("{0} Hr {1} Min" -f $Hours, $Minutes)
+    $minutes = $null; $hours = [math]::divrem($PlayTime, 60, [ref]$minutes);
+    return ("{0} Hr {1} Min" -f $hours, $minutes)
 }
 
 function PlayDateEpochToString($PlayDateEpoch) {
-	[datetime]$origin = '1970-01-01 00:00:00'
-	return $origin.AddSeconds($PlayDateEpoch).ToLocalTime().ToString("dd MMMM yyyy")
+    [datetime]$origin = '1970-01-01 00:00:00'
+    return $origin.AddSeconds($PlayDateEpoch).ToLocalTime().ToString("dd MMMM yyyy")
 }
 
 function PlayTimeStringToMin($PlayTime) {
-	if ( -Not ($PlayTime -match '^[0-9]{0,5} Hr [0-5]{0,1}[0-9]{1} Min$') ) {
+    if ( -Not ($PlayTime -match '^[0-9]{0,5} Hr [0-5]{0,1}[0-9]{1} Min$') ) {
         Log "Error: Incorrect playtime format. Returning null"
         return $null
     }
 
-	$Hours = $PlayTime.Split(" ")[0]
-	$Minutes = $PlayTime.Split(" ")[2]
+    $hours = $PlayTime.Split(" ")[0]
+    $minutes = $PlayTime.Split(" ")[2]
 
-	return ([int]$Hours * 60) + [int]$Minutes
+    return ([int]$hours * 60) + [int]$minutes
 }
 
 function BytesToBitmap($ImageBytes) {
-	$IconByteStream = [System.IO.MemoryStream]::new($ImageBytes)
-	$IconBitmap = [System.Drawing.Bitmap]::FromStream($IconByteStream)
-	return $IconBitmap
+    $iconByteStream = [System.IO.MemoryStream]::new($ImageBytes)
+    $iconBitmap = [System.Drawing.Bitmap]::FromStream($iconByteStream)
+    return $iconBitmap
 }
 
 function ResizeImage($ImagePath, $GameName) {
-	$ImageFileName = ToBase64 $GameName
-	$WIA = New-Object -com wia.imagefile
-	$WIA.LoadFile($ImagePath)
-	$WIP = New-Object -ComObject wia.imageprocess
-	$Scale = $WIP.FilterInfos.Item("Scale").FilterId                    
-	$WIP.Filters.Add($Scale)
-	$WIP.Filters[1].Properties("MaximumWidth") = 140
-	$WIP.Filters[1].Properties("MaximumHeight") = 140
-	$WIP.Filters[1].Properties("PreserveAspectRatio") = $true
+    $imageFileName = ToBase64 $GameName
+    $WIA = New-Object -com wia.imagefile
+    $WIA.LoadFile($ImagePath)
+    $WIP = New-Object -ComObject wia.imageprocess
+    $scale = $WIP.FilterInfos.Item("Scale").FilterId                    
+    $WIP.Filters.Add($scale)
+    $WIP.Filters[1].Properties("MaximumWidth") = 140
+    $WIP.Filters[1].Properties("MaximumHeight") = 140
+    $WIP.Filters[1].Properties("PreserveAspectRatio") = $true
 
-	$ScaledImage = $WIP.Apply($WIA)
-	$ScaledImagePath = "$env:TEMP\GG-{0}-$ImageFileName.png" -f $(Get-Random)
-	$ScaledImage.SaveFile($ScaledImagePath)
-	return $ScaledImagePath
+    $scaledImage = $WIP.Apply($WIA)
+    $scaledImagePath = "$env:TEMP\GG-{0}-$imageFileName.png" -f $(Get-Random)
+    $scaledImage.SaveFile($scaledImagePath)
+    return $scaledImagePath
 }
 
 function CreateMenuItem($Text) {
-    $MenuItem = New-Object System.Windows.Forms.ToolStripMenuItem
-    $MenuItem.Text = "$Text"
+    $menuItem = New-Object System.Windows.Forms.ToolStripmenuItem
+    $menuItem.Text = "$Text"
     
-    return $MenuItem
+    return $menuItem
 }
 
-function CreateMenuSeparator(){
-	return New-Object Windows.Forms.ToolStripSeparator
+function CreateMenuSeparator() {
+    return New-Object Windows.Forms.ToolStripSeparator
 }
 
 function CreateNotifyIcon($ToolTip, $IconPath) {
-	$NotifyIcon = New-Object System.Windows.Forms.NotifyIcon
-	$Icon = [System.Drawing.Icon]::new($IconPath)
-	$NotifyIcon.Text = $ToolTip; 
-	$NotifyIcon.Icon = $Icon;
+    $notifyIcon = New-Object System.Windows.Forms.NotifyIcon
+    $icon = [System.Drawing.Icon]::new($IconPath)
+    $notifyIcon.Text = $ToolTip; 
+    $notifyIcon.Icon = $icon;
 
-	return $NotifyIcon
+    return $notifyIcon
 }
 
 function OpenFileDialog($Title, $Filters, $DirectoryPath = [Environment]::GetFolderPath('Desktop')) {
-	$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+    $fileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
         InitialDirectory = $DirectoryPath
-        Filter = $Filters
-        Title = $Title
+        Filter           = $Filters
+        Title            = $Title
     }
-	return $FileBrowser
+    return $fileBrowser
 }
 
-function ShowMessage($Msg, $Buttons, $Type){
-	[System.Windows.Forms.MessageBox]::Show($Msg,'Gaming Gaiden', $Buttons, $Type)
+function ShowMessage($Msg, $Buttons, $Type) {
+    [System.Windows.Forms.MessageBox]::Show($Msg, 'Gaming Gaiden', $Buttons, $Type)
 }
 
 function UserConfirmationDialog($Title, $Prompt) {
-	return [microsoft.visualbasic.interaction]::MsgBox($Prompt, "YesNo,Question", $Title).ToString()
+    return [microsoft.visualbasic.interaction]::MsgBox($Prompt, "YesNo,Question", $Title).ToString()
 }
 
 function CalculateFileHash ($FilePath) {
-	$FileName = (Get-Item $FilePath).Name
-	Copy-Item $FilePath "$env:TEMP\$FileName"
-	$FileHash = Get-FileHash "$env:TEMP\$FileName"
-	Remove-Item "$env:TEMP\$FileName"
+    $fileName = (Get-Item $FilePath).Name
+    Copy-Item $FilePath "$env:TEMP\$fileName"
+    
+    $fileHash = Get-FileHash "$env:TEMP\$fileName"
+    Remove-Item "$env:TEMP\$fileName"
 
-	return $FileHash.Hash
+    return $fileHash.Hash
 }
 
 function BackupDatabase {
-	Log "Backing up database"
-	
-	$WorkingDirectory = (Get-Location).Path
-	mkdir -f $WorkingDirectory\backups
-	$Timestamp = Get-Date -f "dd-MM-yyyy-HH.mm.ss"
-	
-	Copy-Item ".\GamingGaiden.db" "$env:TEMP\"
-	Compress-Archive "$env:TEMP\GamingGaiden.db" ".\backups\GamingGaiden-$Timestamp.zip"
-	Remove-Item "$env:TEMP\GamingGaiden.db"
+    Log "Backing up database"
+    
+    $workingDirectory = (Get-Location).Path
+    mkdir -f $workingDirectory\backups
+    $timestamp = Get-Date -f "dd-MM-yyyy-HH.mm.ss"
+    
+    Copy-Item ".\GamingGaiden.db" "$env:TEMP\"
+    Compress-Archive "$env:TEMP\GamingGaiden.db" ".\backups\GamingGaiden-$timestamp.zip"
+    Remove-Item "$env:TEMP\GamingGaiden.db"
 
-	Get-ChildItem -Path .\backups -File | Sort-Object -Property CreationTime | Select-Object -SkipLast 5 | Remove-Item
+    Get-ChildItem -Path .\backups -File | Sort-Object -Property CreationTime | Select-Object -SkipLast 5 | Remove-Item
 }
 
 function RunDBQuery ($Query, $SQLParameters = $null) {
-	
-	if ($null -eq $SQLParameters)
-	{
-		$Result = Invoke-SqliteQuery -Query $Query -DataBase ".\GamingGaiden.db"
-	}
-	else
-	{
-		$Result = Invoke-SqliteQuery -Query $Query -DataBase ".\GamingGaiden.db" -SqlParameters $SQLParameters
-	}
-	return $Result
+    if ($null -eq $SQLParameters) {
+        $result = Invoke-SqliteQuery -Query $Query -DataBase ".\GamingGaiden.db"
+    }
+    else {
+        $result = Invoke-SqliteQuery -Query $Query -DataBase ".\GamingGaiden.db" -SqlParameters $SQLParameters
+    }
+    return $result
 }
 
 function CreateForm($Text, $SizeX, $SizeY, $IconPath) {
-	$form = New-Object System.Windows.Forms.Form
-	$form.Text = $Text
-	$form.Size = New-Object Drawing.Size($SizeX, $SizeY)
-	$form.StartPosition = 'CenterScreen'
-	$form.FormBorderStyle = 'FixedDialog'
-	$form.Icon = [System.Drawing.Icon]::new($IconPath)
-	$form.Topmost = $true
-	$form.ShowInTaskbar = $false
+    $form = New-Object System.Windows.Forms.Form
+    $form.Text = $Text
+    $form.Size = New-Object Drawing.Size($SizeX, $SizeY)
+    $form.StartPosition = 'CenterScreen'
+    $form.FormBorderStyle = 'FixedDialog'
+    $form.Icon = [System.Drawing.Icon]::new($IconPath)
+    $form.Topmost = $true
+    $form.ShowInTaskbar = $false
 
-	return $form
+    return $form
 }
 
 function Createlabel($Text, $DrawX, $DrawY) {
-	$label = New-Object System.Windows.Forms.Label
-	$label.AutoSize = $true
-	$label.Location = New-Object Drawing.Point($DrawX, $DrawY)
-	$label.Text = $Text
+    $label = New-Object System.Windows.Forms.Label
+    $label.AutoSize = $true
+    $label.Location = New-Object Drawing.Point($DrawX, $DrawY)
+    $label.Text = $Text
 
-	return $label
+    return $label
 }
 
 function CreateTextBox($Text, $DrawX, $DrawY, $SizeX, $SizeY) {
-	$textBox = New-Object System.Windows.Forms.TextBox
-	$textBox.Text = $Text
-	$textBox.Location = New-Object Drawing.Point($DrawX, $DrawY)
-	$textBox.Size = New-Object System.Drawing.Size($SizeX,$SizeY)
-	
-	return $textBox
+    $textBox = New-Object System.Windows.Forms.TextBox
+    $textBox.Text = $Text
+    $textBox.Location = New-Object Drawing.Point($DrawX, $DrawY)
+    $textBox.Size = New-Object System.Drawing.Size($SizeX, $SizeY)
+    
+    return $textBox
 }
 
 function CreateButton($Text, $DrawX, $DrawY) {
-	$button = New-Object System.Windows.Forms.Button
-	$button.Location = New-Object Drawing.Point($DrawX, $DrawY)
-	$button.Text = $Text
+    $button = New-Object System.Windows.Forms.Button
+    $button.Location = New-Object Drawing.Point($DrawX, $DrawY)
+    $button.Text = $Text
 
-	return $button
+    return $button
 }
 
-function CreatePictureBox($ImagePath, $DrawX, $DrawY, $SizeX, $SizeY){
-	$pictureBox = New-Object Windows.Forms.PictureBox
-	$pictureBox.Location = New-Object Drawing.Point($DrawX, $DrawY)
-	$pictureBox.Size = New-Object Drawing.Size($SizeX, $SizeY)
-	$pictureBox.SizeMode = [System.Windows.Forms.PictureBoxSizeMode]::CenterImage
-	$pictureBox.Image = [System.Drawing.Image]::FromFile($ImagePath)
+function CreatePictureBox($ImagePath, $DrawX, $DrawY, $SizeX, $SizeY) {
+    $pictureBox = New-Object Windows.Forms.PictureBox
+    $pictureBox.Location = New-Object Drawing.Point($DrawX, $DrawY)
+    $pictureBox.Size = New-Object Drawing.Size($SizeX, $SizeY)
+    $pictureBox.SizeMode = [System.Windows.Forms.PictureBoxSizeMode]::CenterImage
+    $pictureBox.Image = [System.Drawing.Image]::FromFile($ImagePath)
 
-	return $pictureBox
+    return $pictureBox
 }
