3c20bff42873069f38ab2098d4c8628fb7dc7f39
Kulvinder Singh <kulvind3r.singh@gmail.com>
2024-05-28 13:47:33 +0530

    Use 'powershell.exe' instead of powershell command
    For certain users on windows 11 or even 10 where powershell versions
    beyond 5.1 are installed, 'powershell' command possibly points to pwsh.exe
    which causes unexpected behavior.
    
    Using powershell.exe explicitly ensures only 5.1 is used when launching the app.

--- Install.bat
+++ Install.bat
@@ -9,14 +9,14 @@ set "IconPath=%InstallDirectory%\icons\running.ico"
 echo Creating Shortcuts
 
 REM Create desktop shortcut using powershell
-powershell -NoProfile -Command "$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%DesktopPath%\Gaming Gaiden.lnk'); $Shortcut.TargetPath = 'powershell'; $Shortcut.Arguments = '-NoLogo -ExecutionPolicy bypass -File \"%InstallDirectory%\GamingGaiden.ps1\"'; $Shortcut.IconLocation = '%IconPath%'; $Shortcut.WorkingDirectory = '%InstallDirectory%'; $Shortcut.WindowStyle = 7; $Shortcut.Save()"
+powershell.exe -NoProfile -Command "$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%DesktopPath%\Gaming Gaiden.lnk'); $Shortcut.TargetPath = 'powershell.exe'; $Shortcut.Arguments = '-NoLogo -ExecutionPolicy bypass -File \"%InstallDirectory%\GamingGaiden.ps1\"'; $Shortcut.IconLocation = '%IconPath%'; $Shortcut.WorkingDirectory = '%InstallDirectory%'; $Shortcut.WindowStyle = 7; $Shortcut.Save()"
 
 REM Copy shortcut to install directory as well
 copy "%DesktopPath%\Gaming Gaiden.lnk" "%InstallDirectory%"
 
 REM Unblock all gaming gaiden files as they are downloaded from internet and blocked by default
 echo Unblocking all Gaming Gaiden files
-powershell -NoProfile -Command "Get-ChildItem '%InstallDirectory%' -Recurse | Unblock-File"
+powershell.exe -NoProfile -Command "Get-ChildItem '%InstallDirectory%' -Recurse | Unblock-File"
 
 set /p AutoStartChoice="Would you like Gaming Gaiden to auto start at boot? Yes/No: "
 
